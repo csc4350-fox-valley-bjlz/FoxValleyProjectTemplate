d6e35964a5bb918673838410dca747e71fcee2c8
ldeagueros01 <ldeagueros01@aurora.edu>
2021-11-29 11:23:07 -0600

    3
    3

--- routes/Form03Routes.js
+++ routes/Form03Routes.js
@@ -4,19 +4,43 @@ const express = require('express');
 // Get router from express
 const router = express.Router();
 
+// Require db file
+const db = require('../db.js');
+
+// Get Sequelize query types
+const { QueryTypes } = require('sequelize');
+const { stringify } = require('querystring');
+
+// Get sql connection
+const con = db.getConnection();
+
 // Return Rendered Submit Minor Injury Log Form
-router.get('/NewSubmission', (req, res) => {
-    res.render('PDRMA_Form_03_Property_Loss_Report');
+router.post('/UpdateForm/:id', (req, res) => {
+    let id = req.params.id;
+    let sql = `SELECT * FROM form_03 WHERE Form03ID = ${id}`;
+    con.query(sql, (err, results, fields) => {
+        res.render('form_03_update', {
+            title: 'test',
+            data: results
+        });
+        console.log(results);
+    });
 });
 
 // Return Rendered Minor Injury Log Form with specific id
-router.get('/:id', (req, res) => {
+//router.get('/:id', (req, res) => {
 
-});
+//});
 
 // Return Rendered Table of Minor Injury Log Forms
 router.get('/TableView', (req, res) => {
-
+    let sql = 'SELECT * FROM form_03';
+    con.query(sql, (err, results, fields) => {
+        res.render('ViewForms04', {
+            title: 'title',
+            data: results
+        });
+    });
 });
 
 // Return Array of JSON objects of Minor Injury Log Forms
@@ -43,5 +67,13 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
 
 });
+
+// router.use(function (err, req, res, next) {
+//     if (err) {
+//       console.log('Error', err);
+//     } else {
+//       console.log('404')
+//     }
+//   });
 
 module.exports = router;
